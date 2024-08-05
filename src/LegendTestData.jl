e87d5619f33e5ce60dc3fd8ce15c9765be7ccb85
Oliver Schulz <oschulz@mpp.mpg.de>
2024-08-05 17:24:19 +0200

    Update legend-test-data artifact

--- src/LegendTestData.jl
+++ src/LegendTestData.jl
@@ -22,7 +22,7 @@ managed via [DataDeps.jl](https://github.com/oxinabox/DataDeps.jl).
 Set `ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"` to avoid interactive prompt
 asking for download permission.
 """
-legend_test_data_path() = joinpath(artifact"legend_testdata", "legend-exp-legend-testdata-e925728")
+legend_test_data_path() = joinpath(artifact"legend_testdata", "legend-exp-legend-testdata-82ad6c2")
 export legend_test_data_path
 
 
